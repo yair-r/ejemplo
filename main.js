4b9c75d0d894799de6810e6f2ca5a1a6d34a5608
yair-r <yair_ro016@hotmail.com>
2023-03-08 14:09:50 -0600

    Primer commit proyecto HTML CSS y JS

---
+++ main.js
@@ -0,0 +1,12 @@
+const tarjeta=document.querySelector('.contenido_tarjeta');
+tarjeta.addEventListener('click',function()
+{
+    tarjeta.classList.toggle('es-volteada')
+}
+)
+const tarjeta2=document.querySelector('.contenido_tarjeta2');
+tarjeta.addEventListener('click',function()
+{
+    tarjeta.classList.toggle('es-rotada')
+}
+)
